765cad8e318566ccb416f267b05e8745f4052387
3219005497郑宝æ?[http] <1097641461@qq.com>
2022-11-27 21:17:26 +0800

    完成了商品列表的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,68 @@
 	export default {
 		data() {
 			return {
+				//请求参数对象
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+				isLoading: false
 				
 			};
+		}, 
+		onLoad(options) {
+			
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			
+			this.getGoodsList()
+		},
+		methods: {
+			//获取商品列表数据
+			async getGoodsList(cb) {
+				this.isLoading = true
+				const {data : res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isLoading = false
+				cb && cb()
+				if (res.meta.status !== 200) return uni.$showMsg()
+				
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoDetail(goods) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+			}
+		},
+		onReachBottom() {
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕')
+			if (this.isLoading) return
+			
+			//让页码自增+1
+			this.queryObj.pagenum++
+			this.getGoodsList()
+			
+		},
+		onPullDownRefresh() {
+			//重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isLoading = false
+			this.goodsList = []
+			
+			this.getGoodsList(() => uni.stopPullDownRefresh())
+			
+			
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	
 </style>
